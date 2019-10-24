8aee829670a1a2af1efe1b9f4cf8fb7d5a5d3ca3
Manu-P-Z <manu.portoles.zagala@gmail.com>
2019-10-24 10:56:57 +0200

    :tada: Commit Inicial 02304-LetraNIFConsola

---
+++ src/org/japo/java/main.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.japo.java;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ *
+ * @author Manu Portolés
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments*
+     */
+    public static final Scanner SCN
+            = new Scanner(System.in, "Windows-1252")
+                    .useLocale(Locale.ENGLISH).useDelimiter("\\s+");
+
+    public static void main(String[] args) {
+      
+        char letraDNI;
+
+        try {
+
+            System.out.print("Letra DNI .............: ");
+
+            letraDNI = SCN.nextLine().charAt(0);
+
+            System.out.printf("%s%C%n", "Letra DNI .............: ", letraDNI);
+
+        } catch (Exception e) {
+
+            System.out.println("ERROR: Entrada incorrecta");
+
+        } finally {
+
+         
+        }
+
+    }
+
+}
